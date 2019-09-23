d1e723043040b5f2e773b4cdda6a14a7d2487a45
DougFunny <douglaseduardo220483@gmail.com>
2019-09-23 17:54:37 -0300

    excercíco 3

---
+++ exercises/3.1/3_1/ex3.js
@@ -0,0 +1,7 @@
+//Faça um programa que retorne o maior de três números. Defina no começo do programa três variáveis com os valores que serão comparados.
+let a, b, c
+a = 10
+b = 5
+c = 91
+console.log("Entre os valores " + a + ", " + b + " e " + c)
+console.log("o maior valor é " + Math.max(a, b, c))
