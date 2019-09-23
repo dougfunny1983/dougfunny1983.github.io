134d31e7516a8426350733051008babe2cc30aaa
DougFunny <douglaseduardo220483@gmail.com>
2019-09-23 19:33:03 -0300

    exercico 8

---
+++ exercises/3.1/3_1/ex8.js
@@ -0,0 +1,15 @@
+//8- Escreva um programa que define três números em variáveis no seu começo e retorna true se uma das três for par. Caso contrário, ele retorna false.
+let a = 11,
+    b = 12,
+    c = 5,
+    i, lista, resposta
+lista = new Array
+lista.push(a, b, c)
+for (i in lista) {
+    if (i / 2 == 1) {
+        resposta = true
+    } else {
+        resposta = false
+    }
+}
+console.log(resposta)
