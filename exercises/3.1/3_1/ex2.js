d393bb00c7b6dfe6cb57054335d8b77de56fd981
DougFunny <douglaseduardo220483@gmail.com>
2019-09-23 17:25:11 -0300

    Excercício 2

---
+++ exercises/3.1/3_1/ex2.js
@@ -0,0 +1,6 @@
+//Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados.
+let a, b, resultado
+a = 10
+b = 91
+resultado = (a > b) ? "o valor " + a + " é o maior" : "o valor " + b + " é o maior"
+console.log("entre o valor " + a + " e " + b + " " + resultado)
