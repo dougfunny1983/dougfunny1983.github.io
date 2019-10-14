6e09f1ab8705e0edc4698a84f1852c560c749c5e
DougFunny <douglaseduardo220483@gmail.com>
2019-10-14 18:42:08 -0300

    escrevendo a função do for

--- exercises/6.1/exercicios6/script.js
+++ exercises/6.1/exercicios6/script.js
@@ -1,10 +1,69 @@
 window.onload = function() {
     console.log("TEste")
-    const regular = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/
+    let regular = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/
     let email = document.querySelector("#email").value
     if (email = ~regular) {
 
     } else {
         alert("digite um email valido")
     }
+
+    let estadosBrasileiros = {
+        'AC': 'Acre',
+        'AL': 'Alagoas',
+        'AP': 'Amapá',
+        'AM': 'Amazonas',
+        'BA': 'Bahia',
+        'CE': 'Ceará',
+        'DF': 'Distrito Federal',
+        'ES': 'Espírito Santo',
+        'GO': 'Goiás',
+        'MA': 'Maranhão',
+        'MT': 'Mato Grosso',
+        'MS': 'Mato Grosso do Sul',
+        'MG': 'Minas Gerais',
+        'PA': 'Pará',
+        'PB': 'Paraíba',
+        'PR': 'Paraná',
+        'PE': 'Pernambuco',
+        'PI': 'Piauí',
+        'RJ': 'Rio de Janeiro',
+        'RN': 'Rio Grande do Norte',
+        'RS': 'Rio Grande do Sul',
+        'RO': 'Rondônia',
+        'RR': 'Roraima',
+        'SC': 'Santa Catarina',
+        'SP': 'São Paulo',
+        'SE': 'Sergipe',
+        'TO': 'Tocantins'
+    }
+    let estado = document.querySelector("#estados")
+
+    function chamarFilho(chave, valor) {
+        let option = document.createElement("option")
+        estado.appendChild(option)
+        option.value = chave
+        option.innerHTML = valor
+    }
+    chamarFilho()
+
+    for (let key in estadosBrasileiros) {
+        chamarFilho(key, estadosBrasileiros[key])
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
