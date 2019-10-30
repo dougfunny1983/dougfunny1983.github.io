c87479dd31bd7d9cb76c7afa19830ac207ae84d3
DougFunny <douglaseduardo220483@gmail.com>
2019-10-30 19:26:59 -0300

    ex 7

--- exercises/8.3/ex6.js
+++ exercises/8.3/ex6.js
@@ -4,17 +4,16 @@ const palio = ["Palio", "Fiat", 2019]
 const shelbyCobra = ["Shelby Cobra", "Ford", 1963]
 const chiron = ["Chiron", "Bugatti", 2016]
 
-function toObject(carro) {
-    return { 'name': carro[0], 'brand': carro[1], 'year': carro[2] }
-}
-// const toObject = ([a,b, c]) =>{
-//     const foda = {
-//         name: a,
-//         brand: b,
-//         year: c
-//     }
-// return foda
-// }
+
+const toObject = ([name, brand, year]) => ({ name, brand, year })
+    // const toObject = ([a,b, c]) =>{
+    //     const foda = {
+    //         name: a,
+    //         brand: b,
+    //         year: c
+    //     }
+    // return foda
+    // }
 
 assert.deepEqual(toObject(palio), { name: "Palio", brand: "Fiat", year: 2019 })
 assert.deepEqual(toObject(shelbyCobra), { name: "Shelby Cobra", brand: "Ford", year: 1963 })
