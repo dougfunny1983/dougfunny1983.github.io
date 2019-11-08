782b55a71a5960e448ed7aa4af809dba27f3237b
DougFunny <douglaseduardo220483@gmail.com>
2019-11-08 19:36:11 -0300

    terminado o ex 2

--- exercises/9.3/apiScript.js
+++ exercises/9.3/apiScript.js
@@ -8,28 +8,31 @@
 //         .then((bread) => buscarId('jokeContainer').innerHTML = bread.joke)
 // }
 // window.onload = () => fetchJoke()
+
+
 const numerosAleatorios = () => {
+
     return new Promise((resolve, reject) => {
-            const aleatorios = () => {
-                    const lista = []
-                    for (let index = 0; index < 9; index++) {
-                        const num = Math.floor(Math.random() * 50 + 1)
-                        lista.push(num)
-                    }
-                    return lista
-                }
-                // const lista = aleatorios()
-
-            if (soma <= 8000) {
-                resolve(aleatorios().reduce((a, b) => a + b, 0))
-            } else {
-                return reject("Isso tá grande de mais sô")
+        const aleatorios = () => {
+            const lista = []
+            for (let index = 0; index < 9; index++) {
+                const num = Math.floor(Math.random() * 50 + 1)
+                lista.push(num)
             }
+            return lista
         }
-
-    }
+        const soma = resolve(aleatorios().reduce((a, b) => a + b, 0))
+        if (soma <= 8000) {
+            resolve(soma)
+        } else {
+            return reject("Isso tá grande de mais sô")
+        }
+    })
 }
 
 
+
+
 numerosAleatorios()
-    .catch((error) => console.log(error))
+    .then((param) => { console.log(param) })
+    .catch((error) => { console.log(error) })
