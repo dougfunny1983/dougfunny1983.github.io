048af304a1f80d3865d091ace033f6ab3fe38b9d
DougFunny <douglaseduardo220483@gmail.com>
2019-11-15 16:53:33 -0300

    TERMINANDO OS EX

---
+++ exercises/10.2/my-pokedex/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import CriarListaPokemon from "./CriarListaPokemon";
+
+
+function App() {
+  return (
+    <div className='Flex-box'>
+    <CriarListaPokemon />
+    </div>
+    )
+}
+
+export default App;
