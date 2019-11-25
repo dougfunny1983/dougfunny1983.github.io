f2e839fc792ceaa1767b6fcff947716d77c1e586
DougFunny <douglaseduardo220483@gmail.com>
2019-11-25 19:08:16 -0300

    Problema aparecendo no this

--- exercises/12.1/exercicos/src/App.js
+++ exercises/12.1/exercicos/src/App.js
@@ -2,11 +2,7 @@ import React, { Component } from 'react'
 import Heroes from './Heroes'
 
 class App extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //   };
-  // }
+
   render() {
     return (
       <Heroes />
--- exercises/12.1/exercicos/src/Heroes.jsx
+++ exercises/12.1/exercicos/src/Heroes.jsx
@@ -2,17 +2,34 @@ import React, { Component } from 'react';
 
 
 class Heroes extends Component {
-  listHero = ['Ursa', 'Sven', 'Luna', 'Phantom Assassin', 'Sniper', 'Girocopter', 'Huskar']
-  creatHero = (value) => {
-    <button key={value} type="button" >{value}</button>
+  constructor(props) {
+    super(props);
+    this.state = {
+      chooseYourFavorite: ''
+    };
   }
+
+  theChosenOneWas = (state) => {
+    this.setState((state) =>({chooseYourFavorite: state}))
+  }
+
+ 
+
+  creatHero = (id, item,funct) => <button key={id} type="button" onClick={()=> funct}>{item}</button>
+
   render() {
+    const listHero = ['Ursa', 'Sven', 'Luna', 'Phantom Assassin', 'Sniper', 'Girocopter', 'Huskar']
+  
+    const newArray = listHero.map((select, index) => ({"id": index +1, "item": select}));
+  
+    console.log(newArray)
+
     return (
       <div>
-        <input type="text" />
-      <div>
-      {this.listHero.map(select => this.creatHero(select))}
-      </div>
+        <input type="text" value={this.state.chooseYourFavorite}/>
+        <div>
+      {newArray.map(select => this.creatHero(select.id,select.item,this.theChosenOneWas(select.item)))}
+        </div>
       </div>
     );
   }
