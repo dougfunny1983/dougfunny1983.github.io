5d0911191426ebb5c5d4a4fda4638ef970cead2a
Douglas Eduardo <douglaseduardo220483@gmail.com>
2019-11-27 11:33:14 -0300

    trazer dimamicamente um componente...

--- exercises/12.2/ex-dia-12-2-parte2/src/App.js
+++ exercises/12.2/ex-dia-12-2-parte2/src/App.js
@@ -1,27 +1,39 @@
 import React, { Component } from 'react';
-import pai from './Father'
 import './App.css';
-
-
+import Doguitchos from './Doguitchos';
+import Button from './Button';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      // lista:[],
     };
   }
+
+  componentDidUpdate() {}
+
+  teste = () => {
+    console.log(<Doguitchos />);
+  };
+  // this.setState({
+
+  // })
+
+  //   render () {
+  //     return Dados.map(select => <CriarPokemon dados={select}/>);
+  // }
+  // }
 
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <button type='button' on></button>
-          
+          <div className="Flex-box">
+            <Button pushImg={this.teste}></Button>
+          </div>
         </div>
       </div>
     );
   }
-  
-  }
-
-
+}
