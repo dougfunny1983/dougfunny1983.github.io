13d844de2e0e1ef29d6b83530ddaf2068358372b
DougFunny <douglaseduardo220483@gmail.com>
2019-11-27 19:26:36 -0300

    Exercico de fixação

--- exercises/12.3/fixation-exercises-12-3/src/App.js
+++ exercises/12.3/fixation-exercises-12-3/src/App.js
@@ -1,11 +1,25 @@
 // arquivo App.js, criado pelo create-react-app, modificado
 import React, { Component } from 'react';
-import Home from './Home';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
+import Home from './header/Home';
+import About from './header/About';
+import User from './header/User';
 
 class App extends Component {
   render() {
     return (
-      <Home />
+      <header>
+        <BrowserRouter>
+          <ul>
+            <li> <Link to="/" > Home </Link> </li>
+            <li> <Link to="/about" >About </Link> </li>
+            <li> <Link to="/user" > User </Link> </li>
+          </ul>  
+          <Route exact path="/" component={Home} />
+          <Route path="/About" component={About} />
+          <Route path="/User" component={User} />
+        </BrowserRouter>
+      </header>
     );
   }
 }
