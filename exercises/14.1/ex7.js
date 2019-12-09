e521196c2700fbe7b5d5e9f420fd4e0304cd788f
Douglas Eduardo <douglaseduardo220483@gmail.com>
2019-12-09 18:57:29 -0300

    ex 7 ok

---
+++ exercises/14.1/ex7.js
@@ -0,0 +1,12 @@
+let assert = require('assert')
+var teste
+function name(params) {
+}
+
+
+var x = { a: 1, b: 2 };
+var y = { b: 2, a: 1 };
+
+assert.deepEqual(x, y)
+assert.ok(teste === undefined)
+assert.ok(typeof (name) === 'function')
