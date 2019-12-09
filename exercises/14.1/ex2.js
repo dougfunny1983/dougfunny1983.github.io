c1979d21aeac6ecc117baaf6b7903b4fb80a759c
Douglas Eduardo <douglaseduardo220483@gmail.com>
2019-12-09 17:08:10 -0300

    Termondo o ex 2

---
+++ exercises/14.1/ex2.js
@@ -0,0 +1,9 @@
+let assert = require('assert')
+
+function myIndexOf(arr, item) {
+  return arr.findIndex(element => { return element === item })
+}
+
+// implemente seus testes aqui
+assert.deepEqual(myIndexOf([1, 2, 3, 4], 3), 2)
+assert.deepEqual(myIndexOf([1, 2, 3, 4], 5), -1)
